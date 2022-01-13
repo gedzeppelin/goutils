556c78f0fc073f977769da4c15added01d5f664f
Gedy Palomino <36518098+gedzeppelin@users.noreply.github.com>
2022-01-13 07:26:33 +0000

    feat: add base utils

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gedzeppelin/go-pulumi-utils
+
+go 1.14
+
+require github.com/pulumi/pulumi/sdk/v3 v3.22.0 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func StackNamer(ctx *pulumi.Context) func(name string) string {
+	return func(name string) string {
+		return fmt.Sprintf("%s/%s", name, ctx.Stack())
+	}
+}
+
+func ResNamer(ctx *pulumi.Context) func(name string) string {
+	return func(name string) string {
+		return fmt.Sprintf("%s-%s", name, ctx.Stack())
+	}
+}
