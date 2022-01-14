1f31b06a12a161324df6d1852d7ba398ea0f97e0
Gedy Palomino <gedy.palomino@gmail.com>
2022-01-14 17:23:58 -0500

    renamed

---
+++ pulumiutils/go.mod
@@ -0,0 +1,5 @@
+module github.com/gedzeppelin/goutils/pulumi
+
+go 1.14
+
+require github.com/pulumi/pulumi/sdk/v3 v3.22.1
--- pulumiutils/pulumiutils.go
+++ pulumiutils/pulumiutils.go
@@ -1,14 +1,27 @@
 package pulumiutils
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func StackNamer(ctx *pulumi.Context) func(name string) string {
+	c := context.Background()
+	stack, err := auto.UpsertStackInlineSource(c, ctx.Stack(), ctx.Project(), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	acc, err := stack.Workspace().WhoAmI(c)
+	if err != nil {
+		panic(err)
+	}
+
 	return func(name string) string {
-		return fmt.Sprintf("%s/%s", name, ctx.Stack())
+		return fmt.Sprintf("%s/%s/%s", acc, name, ctx.Stack())
 	}
 }
 
