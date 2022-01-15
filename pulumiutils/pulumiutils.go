efe6637ac9d9c4aa883db135a23a042843a91a83
Gedy Palomino <gedy.palomino@gmail.com>
2022-01-15 03:14:30 -0500

    feat: update pulumiutils

--- pulumiutils/pulumiutils.go
+++ pulumiutils/pulumiutils.go
@@ -8,25 +8,36 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func StackNamer(ctx *pulumi.Context) func(name string) string {
-	c := context.Background()
-	stack, err := auto.UpsertStackInlineSource(c, ctx.Stack(), ctx.Project(), nil)
+func ProjectStackNamer(pulumiCtx *pulumi.Context) func(name string) string {
+	ctx := context.Background()
+	stack := pulumiCtx.Stack()
+
+	workspace, err := auto.SelectStackInlineSource(ctx, stack, pulumiCtx.Project(), nil)
 	if err != nil {
 		panic(err)
 	}
 
-	acc, err := stack.Workspace().WhoAmI(c)
+	acc, err := workspace.Workspace().WhoAmI(ctx)
 	if err != nil {
 		panic(err)
 	}
 
 	return func(name string) string {
-		return fmt.Sprintf("%s/%s/%s", acc, name, ctx.Stack())
+		return fmt.Sprintf("%s/%s/%s", acc, name, stack)
 	}
+
 }
 
-func ResNamer(ctx *pulumi.Context) func(name string) string {
-	return func(name string) string {
-		return fmt.Sprintf("%s-%s", name, ctx.Stack())
+func ResourceStackNamer(ctx *pulumi.Context) func(name string) string {
+	stack := ctx.Stack()
+
+	if stack == "main" {
+		return func(name string) string {
+			return name
+		}
+	} else {
+		return func(name string) string {
+			return fmt.Sprintf("%s-%s", name, ctx.Stack())
+		}
 	}
 }
